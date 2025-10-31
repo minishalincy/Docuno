49dfc1b5dace24106ef2ae713832531ccd22b1af
unknown <minishalincy123@gmail.com>
2025-10-31 22:23:52 +0530

    fixes

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,43 +1,49 @@
-import React from 'react'
-import { assets } from '../assets/assets_frontend/assets'
+import React from "react";
+import { assets } from "../assets/assets_frontend/assets";
 
 const Footer = () => {
-    return (
-        <div className='md:mx-10'>
-            <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm' > 
-                {/* ----left section */}
-                <div >
-                    <img className='mb-5 w-40' src={assets.logo} alt="" />
-                    <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-
-                </div>
-                {/* ----center section */}
-                <div>
-                    <p className='text-xl font-medium mb-5'>COMPANY</p>
-                    <ul className='flex flex-col gap-2 text-gray-600'>
-                        <li>Home</li>
-                        <li>About us</li>
-                        <li>Contact Us</li>
-                        <li>Privacy policy</li>
-                    </ul>
-
-                </div>
-                {/* ----right section */}
-                <div>
-                    <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
-                    <ul className='flex flex-col gap-2 text-gray-600'>
-                        <li>+111-2222-333-4</li>
-                        <li>docuno@gmail.com</li>
-                    </ul>
-                </div>
-            </div>
-            {/* copyright text */}
-            <div>
-                <hr />
-                <p className='py-5 text-sm text-center' >Copyright © 2025 MinishaLincy - All Right Reserved</p>
-            </div>
+  return (
+    <div className="md:mx-10">
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+        {/* ----left section */}
+        <div>
+          <img className="mb-5 w-40" src={assets.logo} alt="" />
+          <p className="w-full md:w-2/3 text-gray-600 leading-6">
+            About Docuno - Your trusted platform for booking doctor appointments
+            with ease. Connect with qualified specialists, view real-time
+            availability, and manage your health effortlessly. We make
+            healthcare access simpler, faster, and more reliable – all in one
+            place.
+          </p>
         </div>
-    )
-}
+        {/* ----center section */}
+        <div>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-2 text-gray-600">
+            <li>Home</li>
+            <li>About us</li>
+            <li>Contact Us</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+        {/* ----right section */}
+        <div>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-2 text-gray-600">
+            <li>+111-2222-333-4</li>
+            <li>docuno@gmail.com</li>
+          </ul>
+        </div>
+      </div>
+      {/* copyright text */}
+      <div>
+        <hr />
+        <p className="py-5 text-sm text-center">
+          Copyright © 2025 MinishaLincy - All Right Reserved
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
                 </p>
                 <div className='flex flex-col md:flex-row items-center gap-3 text-white text-sm fint-light'>
                     <img className='w-20' src={assets.group_profiles} />
-                    <p>Simply browse through our extensive list of trusted doctors,<br className='hidden sm:block' />
+                    <p>Simply browse through our extensive list of trusted doctors,
                         schedule your appointment hassle-free.</p>
                 </div>
                 <a href='#speciality' className='flex items-center gap-3 bg-white px-8 py-3 rounded-full text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300'>
