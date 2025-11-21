80c5baee052493e1c743fba88c3dd5026830caf7
X S Minisha Alias Lincy <minishalincy123@gmail.com>
2025-11-21 20:54:39 +0530

    imporve code readability

--- backend/middlewares/authAdmin.js
+++ backend/middlewares/authAdmin.js
@@ -4,26 +4,18 @@ import jwt from 'jsonwebtoken';
 const authAdmin = async (req, res, next) => {
   try {
     const atoken = req.headers.atoken;
-
-    console.log("👉 Received atoken:", atoken);
-
     if (!atoken) {
       return res.json({ success: false, message: "Not Authorized Login Again" });
     }
 
     const token_decode = jwt.verify(atoken, process.env.JWT_SECRET);
-    console.log("✅ Decoded token:", token_decode);
 
     if (token_decode.email !== process.env.ADMIN_EMAIL) {
-      console.log("❌ Mismatch! Email did not match.");
       return res.json({ success: false, message: "Not Authorized Login Again" });
     }
-
-    console.log("✅ Token is valid! Continuing...");
     next();
 
   } catch (error) {
-    console.log("❌ JWT Error:", error);
     res.json({ success: false, message: error.message });
   }
 };
